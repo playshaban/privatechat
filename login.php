a1fa20a48109481d2e37bb82ab2b8dcc37379f62
Shaban Khan <74647853+playshaban@users.noreply.github.com>
2021-11-08 22:28:15 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 $uname   = $_POST['username'];
 $pass = $_POST['password'];
 
-$_SESSION['uid'] = $uname;
-?> <script> location.href='new.php' ; </script> <?php
+echo "hello".uname;
+echo "welcome to the page";
 
 ?>
